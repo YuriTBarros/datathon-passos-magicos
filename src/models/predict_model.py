253c96eb6f5c82d3cbdfe3aae7fd632221916430
Yuri T. de Barros <65117064+YuriTBarros@users.noreply.github.com>
2026-01-18 10:12:17 -0700

    Criando estrutura base do projeto com Esteira CI inicial

---
+++ src/models/predict_model.py
@@ -0,0 +1,8 @@
+def carregar_modelo():
+    # Funcao dummy para carregar o modelo
+    return "Modelo Dummy"
+
+def prever_risco(dados_aluno, modelo):
+    # funcao de retorno esperado para API(inicialmente dummy).
+    # Tavez precise de ajuste no futuro
+    return {"risco": True, "probabilidade": 0.85}
